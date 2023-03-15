a5ae12428a8d7031ad8155b77f784c2f01bd023e
Divyanshu <groverdivyanshu97@gmail.com>
2023-03-15 14:05:35 +0530

    added some files

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,4 +1,6 @@
 module.exports.home=function(req,res){
 
-    return res.end("<h1>Hello the server is ruunig</h1");
+    return res.render('home',{
+        title:"divyanshu",
+    })
 }
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const expresslayout=require('express-ejs-layouts');
+app.use(expresslayout);
+app.use(express.static('./assets'))
+app.set('layout extractStyles', true);
+app.set('layout extractScripts',true);
 
-
+//use express router
 app.use('/',require('./routes'));
+
+
+//set view engine and view
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err)
